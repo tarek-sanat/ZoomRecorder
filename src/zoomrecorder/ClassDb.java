7859af6f72fb161b32cbdf3b14439e6de8d0fc16
tarek <tarek@DESKTOP-0LAAC7V>
2021-01-16 18:26:26 -0500

    Change the time when the program triggers

--- src/zoomrecorder/ClassDb.java
+++ src/zoomrecorder/ClassDb.java
@@ -51,6 +51,7 @@ public class ClassDb {
         return Integer.parseInt(dbConnect("ClassDuration"));
     }
     
+    // get the id of the class
     public String getId(){
         return dbConnect("ClassId");
     }
